49c1a7cc78a67ea17fe221e5b7e8b3fed61c3212
SYMPH373 <1.h.o.r.s.e.m.e.t.a@gmail.com>
2024-12-16 10:32:34 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  }
+};
+
+module.exports = nextConfig; 
