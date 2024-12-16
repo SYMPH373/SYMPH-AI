a237af45df2781f8e3508c617848765d40a5b8e2
SYMPH373 <1.h.o.r.s.e.m.e.t.a@gmail.com>
2024-12-16 10:38:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,14 @@ const nextConfig = {
   },
   experimental: {
     forceSwcTransforms: true
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      fs: false, 
+      net: false, 
+      tls: false 
+    };
+    return config;
   }
 };
 
