a65e3165ae4bf437ae923a12daab76babb725f40
SYMPH373 <1.h.o.r.s.e.m.e.t.a@gmail.com>
2025-01-07 04:31:41 +0800

    Update index.tsx

--- src/components/TransactionDetails/index.tsx
+++ src/components/TransactionDetails/index.tsx
@@ -12,23 +12,23 @@ export const TransactionDetails = ({ transaction }: TransactionDetailsProps) => 
   return (
     <div className="transaction-details">
       <div className="transaction-field">
-        <span className="terminal-text-dim">$ signature:</span>
+        <span className="terminal-text-dim">signature:</span>
         <span className="transaction-signature">{transaction.signature}</span>
       </div>
       <div className="transaction-field">
-        <span className="terminal-text-dim">$ type:</span>
+        <span className="terminal-text-dim">type:</span>
         <span className="transaction-type">{transaction.type}</span>
       </div>
       <div className="transaction-field">
-        <span className="terminal-text-dim">$ status:</span>
+        <span className="terminal-text-dim">status:</span>
         <span className="transaction-status">{transaction.status}</span>
       </div>
       <div className="transaction-field">
-        <span className="terminal-text-dim">$ timestamp:</span>
+        <span className="terminal-text-dim">timestamp:</span>
         <span className="transaction-timestamp">{transaction.timestamp}</span>
       </div>
       <div className="transaction-field">
-        <span className="terminal-text-dim">$ fee:</span>
+        <span className="terminal-text-dim">fee:</span>
         <span className="transaction-fee">{transaction.fee} SOL</span>
       </div>
     </div>
