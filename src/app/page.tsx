e46f7514362128bc1f579380aabdc2c9c2348510
SYMPH373 <1.h.o.r.s.e.m.e.t.a@gmail.com>
2025-01-07 01:27:14 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
 
   const handleTokenAddressChange = (address: string) => {
     setTokenAddress(address);
+    (blockchainService as any).tokenAddress = address;
   };
 
   const handleTransactionSelect = useCallback(async (signature: string) => {
